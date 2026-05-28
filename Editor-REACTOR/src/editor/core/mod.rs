4c56b0c7dd8513dd20206e00abc5ac0e2e84a4b6
eddi.salazar.dev <eddi.salazar.dev@gmail.com>
2026-05-27 22:28:24 -0500

    cambios

--- Editor-REACTOR/src/editor/core/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod command_system;
-pub mod editor_context;
-pub mod event_bus;
--- Editor-REACTOR/src/editor/ui/panels/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod asset_browser_panel;
-pub mod console_panel;
-pub mod hierarchy_panel;
-pub mod inspector_panel;
-pub mod toolbar_panel;
-pub mod viewport_panel;
